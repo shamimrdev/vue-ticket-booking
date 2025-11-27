74ab6d65ff826161c95a7e6435497efa4e5fee1d
smshamimsr <mdshamimreza559@gmail.com>
2025-11-27 19:59:15 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Ticket Booking App
+
+A simple ticket booking application built with Vue.js and Tailwind CSS.
+
+## Features
+
+*   Select and deselect seats.
+*   View total price.
+*   Apply coupons.
+*   Confirm booking.
+
+## Setup
+
+1.  Clone the repository.
+2.  Open `index.html` in your browser.
+
+## Usage
+
+1.  Select up to 3 seats.
+2.  Enter a coupon code (optional).
+3.  Enter your name and mobile number.
+4.  Click "Confirm" to book the tickets.
---
+++ style/app.js
@@ -0,0 +1,248 @@
+var app = Vue.createApp({
+  data() {
+    return {
+      couponCode: "",
+      appliedCoupon: null,
+      name: "",
+      mobile: "",
+      confirmed: false,
+      selectedSeat: [],
+      coupons: [
+        {
+          code: "100TAKAOFF",
+          discount: 100,
+        },
+        {
+          code: "200TAKAOFF",
+          discount: 200,
+        },
+      ],
+      seatStates: {
+        sold: {
+          text: "Sold",
+          color: "#ff0000",
+        },
+        available: {
+          text: "Available",
+          color: "#fff",
+        },
+        booked: {
+          text: "Booked",
+          color: "gray",
+        },
+        selected: {
+          text: "Selected",
+          color: "#00ff00",
+        },
+      },
+      seats: [
+        {
+          name: "A1",
+          type: "available",
+          price: 500,
+        },
+        {
+          name: "A2",
+          type: "available",
+          price: 500,
+        },
+        {
+          name: "A3",
+          type: "available",
+          price: 500,
+        },
+        {
+          name: "A4",
+          type: "available",
+          price: 500,
+        },
+        {
+          name: "B1",
+          type: "available",
+          price: 450,
+        },
+        {
+          name: "B2",
+          type: "available",
+          price: 450,
+        },
+        {
+          name: "B3",
+          type: "available",
+          price: 450,
+        },
+        {
+          name: "B4",
+          type: "available",
+          price: 450,
+        },
+        {
+          name: "C1",
+          type: "sold",
+          price: 500,
+        },
+        {
+          name: "C2",
+          type: "sold",
+          price: 500,
+        },
+        {
+          name: "C3",
+          type: "sold",
+          price: 500,
+        },
+        {
+          name: "C4",
+          type: "sold",
+          price: 500,
+        },
+        {
+          name: "D1",
+          type: "available",
+          price: 400,
+        },
+        {
+          name: "D2",
+          type: "available",
+          price: 400,
+        },
+        {
+          name: "D3",
+          type: "available",
+          price: 400,
+        },
+        {
+          name: "D4",
+          type: "available",
+          price: 400,
+        },
+        {
+          name: "E1",
+          type: "available",
+          price: 300,
+        },
+        {
+          name: "E2",
+          type: "available",
+          price: 300,
+        },
+        {
+          name: "E3",
+          type: "booked",
+          price: 300,
+        },
+        {
+          name: "E4",
+          type: "booked",
+          price: 300,
+        },
+        {
+          name: "F1",
+          type: "available",
+          price: 300,
+        },
+        {
+          name: "F2",
+          type: "available",
+          price: 300,
+        },
+        {
+          name: "F3",
+          type: "available",
+          price: 300,
+        },
+        {
+          name: "F4",
+          type: "available",
+          price: 300,
+        },
+      ],
+    };
+  },
+
+  computed: {
+    selected() {
+      let sc = this.seats.filter((item) => item.type === "selected");
+      return sc;
+    },
+
+    totalPrice() {
+      let tc = 0;
+      this.selected.forEach((seat) => {
+        tc += seat.price;
+      });
+
+      if (this.appliedCoupon) {
+        tc -= this.appliedCoupon;
+      }
+      return tc;
+    },
+  },
+
+  methods: {
+    selectedSeats(seat) {
+      let checkSeat = this.seats.find((s) => s.name === seat.name);
+
+      if (checkSeat.type === "sold") {
+        alert("You can not select this seat. This seat is already sold.");
+        return;
+      }
+
+      if (checkSeat.type === "booked") {
+        alert("You can not select this seat. This seat is already booked.");
+        return;
+      }
+
+      if (checkSeat.type == "available" && this.selectedSeat.length > 2) {
+        alert("You can not select more than 3 seats");
+        return;
+      }
+
+      if (checkSeat.type === "selected") {
+        checkSeat.type =
+          checkSeat.type === "selected" ? "available" : "selected";
+        this.selectedSeat.pop(checkSeat);
+        return;
+      }
+      checkSeat.type = checkSeat.type === "selected" ? "available" : "selected";
+
+      this.selectedSeat.push(checkSeat);
+    },
+
+    confirmBooking() {
+      if (!this.name || !this.mobile) {
+        alert("Please provide your name and mobile number");
+        return;
+      }
+
+      this.confirmed = true;
+    },
+    resetData() {
+      this.confirmed = false;
+      this.name = "";
+      this.mobile = "";
+      this.appliedCoupon = null;
+
+      this.seats.forEach((seat) => {
+        if (seat.type === "selected") {
+          seat.type = "sold";
+        }
+      });
+    },
+  },
+
+  watch: {
+    couponCode(newValue) {
+      if (newValue.length === 10) {
+        let foundCoupon = this.coupons.find((c) => c.code === newValue);
+
+        if (foundCoupon) {
+          this.appliedCoupon = foundCoupon.discount;
+        }
+      }
+    },
+    
+  },
+});
+
+app.mount("#app");
